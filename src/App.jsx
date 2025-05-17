97242badea18e7aaa6396b4c3137c9a438780f6b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-16 22:02:06 -0400

    Added form to page with input and submit button

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,25 @@ function App() {
   }
 
   return (
-    <div>
-      App
-    </div>
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={color} 
+            onChange={(e) => setColor(e.target.value)} 
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>          
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+
   )
 }
 
