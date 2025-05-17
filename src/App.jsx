02fa3ce1e8dd0bba50097f942a6d545fd901332e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-16 21:51:53 -0400

    Set up state constants and submit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import './App.css'
+import { useState } from 'react';
+import Values from 'values.js';
 
 function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
 
   return (
     <div>
