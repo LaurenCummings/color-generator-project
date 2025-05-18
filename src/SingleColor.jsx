435a6527dd6d678662ab4d410a3d0623ddc06787
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-17 22:59:09 -0400

    Created hexValue with # included

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -3,11 +3,12 @@ import { useState, useEffect } from 'react';
 function SingleColor({rgb, weight, index, hexColor}) {
     const [alert, setAlert] = useState(false);
     const bcg = rgb.join(',');
+    const hexValue = `#${hexColor}`
 
     return (
         <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
             <p className="percent-value">{weight}%</p>
-            <p className="color-value">{hexColor}</p>
+            <p className="color-value">{hexValue}</p>
         </article>
     )
 }
