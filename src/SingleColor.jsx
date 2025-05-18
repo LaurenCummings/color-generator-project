90c6a9519efe71a75496ec3e7c511a924c9d0e5e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-17 22:09:03 -0400

    Added constant to turn arrays into rgb values

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import rgbToHex from './utils';
 
 function SingleColor({rgb, weight, index}) {
+    const [alert, setAlert] = useState(false);
+    const bcg = rgb.join(',');
+    console.log(bcg);
     return (
         <div>
             SingleColor
