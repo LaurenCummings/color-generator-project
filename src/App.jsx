1c2f91167efbb3ad72e1fba79d1f6c235c608f34
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-17 21:09:16 -0400

    Added conditional error class on input box

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ function App() {
             value={color} 
             onChange={(e) => setColor(e.target.value)} 
             placeholder="#f15025"
+            className={`${error ? 'error' : null}`}
           />
           <button className="btn" type="submit">
             submit
